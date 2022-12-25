e34391a0614e29529ec6227bf6f921f2714bf517
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2022-12-26 01:15:24 +0530

    Update myscript.sh

--- .github/script/myscript.sh
+++ .github/script/myscript.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ssh -i ~/.ssh/yourkey.pem ec2-user@ec2-15-206-146-197.ap-south-1.compute.amazonaws.com 'aws s3 cp s3://shellscript/shell-script.sh - | bash'
+pwd
+cd .github/script
+# ssh -i ~/.ssh/yourkey.pem ec2-user@ec2-15-206-146-197.ap-south-1.compute.amazonaws.com 'aws s3 cp s3://shellscript/shell-script.sh - | bash'
